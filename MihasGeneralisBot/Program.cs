54f0afb1f1bf9891ffd38f70aaa347d870646b96
Zimnuk <Zimnuk@outlook.com>
2020-03-20 17:13:38 +0400

    text changes

--- MihasGeneralisBot/Program.cs
+++ MihasGeneralisBot/Program.cs
@@ -12,26 +12,28 @@ namespace MihasGeneralisBot
     class Program
     {
         private static ITelegramBotClient botClient;
+
         static void Main(string[] args)
         {
             var proxy = new HttpToSocks5Proxy("213.136.89.190", 38816);
             proxy.ResolveHostnamesLocally = true;
-            botClient = new TelegramBotClient("1083411004:AAFGzhy_5CIJkpnyT1TcHjRsOKY2472TEgc", proxy) ;
-            
+            botClient = new TelegramBotClient("1083411004:AAFGzhy_5CIJkpnyT1TcHjRsOKY2472TEgc", proxy);
+
             var me = botClient.GetMeAsync().Result;
             Console.WriteLine(
-              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
-            );
+                $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+                );
 
 
-                botClient.OnCallbackQuery += Bot_OnCallbackQuery;
-                botClient.OnMessage += Bot_OnMessage;
-                botClient.StartReceiving();
-                Thread.Sleep(10000);
+            botClient.OnCallbackQuery += Bot_OnCallbackQuery;
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(10000);
 
             Console.ReadKey();
-            
+
         }
+
         private static async void Bot_OnCallbackQuery(object sender, CallbackQueryEventArgs e)
         {
             string buttonText = e.CallbackQuery.Data;
@@ -39,8 +41,7 @@ namespace MihasGeneralisBot
             Console.WriteLine($"{name} press the button {buttonText}");
             botClient.OnMessage -= Bot_OnMessage;
             botClient.OnMessage += Bot_OnMessageAfterButton;
-            await botClient.SendTextMessageAsync(e.CallbackQuery.From.Id, @$"Now please enter the counter number and indications as shown in the example.  
-                        Example:12345 045566.9"); 
+            await botClient.SendTextMessageAsync(e.CallbackQuery.From.Id, "Please, enter the meter number located near the meter screen and value showed on the screen of the meter reader. \r\n Example:  “12345 045566.9” where “12345” is a meter id and “045566.9” is a meter reading in kWh"); 
         }
 
         private static async void Bot_OnMessageAfterButton(object sender, MessageEventArgs e)
@@ -59,7 +60,7 @@ namespace MihasGeneralisBot
                 indicators.dateOfIndicate = dateTime.ToShortDateString();
                 indicators.sendIndicators("D://indicators");
                 text =
-                    @$"You enter the counter number:'{messageCut[0]}' and indications:{messageCut[1]}";
+                    $"You entered the following information:\r\nMeterId:'{messageCut[0]}' \r\nValue:{messageCut[1]}\r\nTimestemp:'{DateTime.Now}'\r\n";
                 await botClient.SendTextMessageAsync(e.Message.Chat, text);
                 botClient.OnMessage -= Bot_OnMessageAfterButton;
                 botClient.OnMessage += Bot_OnMessage;
@@ -90,10 +91,10 @@ namespace MihasGeneralisBot
                 
                 
                 case "/start":
-                    text = @"You wont to start indicate?";
+                    text = @"Please, confirm you are located beside the meter reader";
                     var inlineKeyboard = new InlineKeyboardMarkup(new[]
                     {
-                        InlineKeyboardButton.WithCallbackData("start indicate"),
+                        InlineKeyboardButton.WithCallbackData("Yes, I am here"),
 
                     });
                     await botClient.SendTextMessageAsync(e.Message.Chat, text, replyMarkup: inlineKeyboard);
